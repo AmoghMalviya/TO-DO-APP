59d54b872056a75373a0d3106188a698d31f226d
AmOGhMaLvIyA <amoghmalviya@gmail.com>
2023-12-05 23:35:15 +0530

    first step logic building for to do app part 1

---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+
+const TodoList = ({ tasks, toggleComplete, deleteTask }) => {
+    return (
+        <>
+            <div>
+                {}
+            </div>
+        </>
+    )
+}
